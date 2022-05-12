6bdc2d88cd1a36b02eb97c77df64a2d92fdf8e32
Paul Curren <paul.curren@necsws.com>
2022-05-12 12:46:06 +0100

    monkey patch Bank

--- tests/test_bank_report.py
+++ tests/test_bank_report.py
@@ -1,35 +1,45 @@
+from datetime import datetime
 import pytest
 
-from bank_api.bank import Bank
+from bank_api.bank import Account, Bank, Transaction
 from bank_api.bank_report import BankReport
 
-@pytest.fixture
-def bank() -> Bank:
-    return Bank()
 
-def test_report_no_transactions(bank: Bank):
-
+def test_report_no_transactions(monkeypatch):
+    bank = Bank()
     bank_report = BankReport(bank)
 
-    bank.create_account('Name 1')
+    def mock_acc(name):
+        return Account(name)
+ 
+    monkeypatch.setattr(bank, 'get_account', mock_acc)
 
     balance = bank_report.get_balance('Name 1')
 
     assert balance == 0
 
-def test_report_1_transactions(bank: Bank):
-
+def test_report_1_transactions(monkeypatch):
+    bank = Bank()
     bank_report = BankReport(bank)
 
-    bank.create_account('Name 1')
-    bank.add_funds('Name 1', 1)
+    def mock_acc(name):
+        return Account(name)
+
+    def mock_tx():
+        transactions = []
+        transactions.append(Transaction(Account('Name 1'), datetime.now(), 10))
+        transactions.append(Transaction(Account('Name 1'), datetime.now(), 20))
+        return transactions
+
+    monkeypatch.setattr(bank, 'get_account', mock_acc)
+    monkeypatch.setattr(bank, 'get_transactions', mock_tx)
 
     balance = bank_report.get_balance('Name 1')
 
-    assert balance == 1
+    assert balance == 30
 
-def test_report_2_transactions(bank: Bank):
-
+def test_report_2_transactions():
+    bank = Bank()
     bank_report = BankReport(bank)
 
     bank.create_account('Name 1')
